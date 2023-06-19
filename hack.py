ce593c9fee00889bfb7510a61715d194a4c48a3d
Deoliveira_oficial <97267995+Euliveira@users.noreply.github.com>
2023-06-19 16:14:50 -0300

    Update hack.py

--- hack.py
+++ hack.py
@@ -7,17 +7,19 @@ os.system('clear')
 time.sleep(1)
 os.system('figlet Hacking')
 time.sleep(1)
-os.system('figlet Willian')
+os.system('figlet Deoliveira')
 time.sleep(3)
 
 print("""
 ••••••••••••••••••••••••••••••••••••••••••••••
       DESENVOLVEDOR: WILLIAN DE OLIVEIRA
-             HACKING NAO EH CRIME
+             HACKING NAO É CRIME
 •••••••••••••••••••••••••••••••••••••••••••••""")
 time.sleep(1)
 IP = input('Digite o IP: ')
 print('IP digitado: ', IP)
+ports = [21,22,80,144,8080]
+
 time.sleep(1)
 print('Conectando...')
 time.sleep(1)
@@ -34,8 +36,6 @@ time.sleep(1)
 print(""" Pronto para uso""")
 time.sleep(2)
 
-ports = [22,80]
-
 print('Conexao bem sucedida')
 time.sleep(1)
 print('Varredura de portas')
@@ -45,25 +45,28 @@ for port in ports:
           client.settimeout(0.1)
           code = client.connect_ex((IP, port))
           if code == 0:
-                  print('Porta aberta')
+                  print(ports,'Porta aberta')
           else:
-                  print('Porta fechada')
+                  print(ports,'Porta fechada')
 
-time.sleep(2)
-os.system('clear')
-time.sleep(1)
-os.system('figlet ,Ataque')
+time.sleep(3)
+os.system('figlet Ataque')
 
 print('IP digitado: ', IP)
+time.sleep(1)
+print('Portas selecionadas: ', ports)
 time.sleep(3)
 
-Pergunta = input('Deseja fazer um ataque DDoS? ')
-if 'responder' == 'y' or 0:
+Question = input('Deseja fazer um ataque DDoS? ')
+if 'response' == 'y' or '1':
     print('Iniciando ataque DDoS')
-
+else:
+      print('O ataque foi cancelado')
 time.sleep(2)
 
 for x in range(10000):
       s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
       bytes = random._urandom(10000)
       print('Ataque ao IP', IP)
+      print(ports)
+      
